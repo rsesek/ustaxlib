b4378cb2f2e7b1f58ef3a64d3c7b1f2d9427b19a
Robert Sesek <rsesek@bluestatic.org>
2020-03-23 00:10:55 -0400

    Add a build script to help create the package.
    NPM "helpfully" uses .gitignore, which excludes dist/, but dist/ needs
    to make it into the NPM tarball. Rewrite a line in the copied
    package.json to explicitly include the dist/ files.

---
+++ build.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+# Copyright 2020 Blue Static <https://www.bluestatic.org>
+# This program is free software licensed under the GNU General Public License,
+# version 3.0. The full text of the license can be found in LICENSE.txt.
+# SPDX-License-Identifier: GPL-3.0-only
+
+OUTDIR=./dist
+
+set -ex
+
+# Clean.
+rm -rf $OUTDIR
+
+# Compile.
+tsc
+
+# Drop tests from the compiled output.
+find $OUTDIR -type f -name '*.test.*' -exec rm {} +
+
+# "Preprocess" the dist package.json.
+cp ./package.json $OUTDIR
+sed -i '' -e s@\"dist/@\"@ $OUTDIR/package.json
