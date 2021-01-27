fc12658bc1cf63d91b020c2bdfbba48ab04607aa
Robert Sesek <rsesek@bluestatic.org>
2021-01-26 23:22:09 -0500

    Use specific reexports from fed2019 into fed2020.
    Also annotate which forms will need to be updated for 2020.

--- src/fed2020/index.ts
+++ src/fed2020/index.ts
@@ -3,6 +3,32 @@
 // version 3.0. The full text of the license can be found in LICENSE.txt.
 // SPDX-License-Identifier: GPL-3.0-only
 
-export * from '../fed2019';
+export { default as Form1040 } from '../fed2019/Form1040';  // TODO - revise for 2020
+export { default as Form1099B } from '../fed2019/Form1099B';
+export { default as Form1099DIV } from '../fed2019/Form1099DIV';
+export { default as Form1099INT } from '../fed2019/Form1099INT';
+export { default as Form1099R } from '../fed2019/Form1099R';
+export { default as Form1116 } from '../fed2019/Form1116';  // TODO - revise for 2020
+export { default as Form6251 } from '../fed2019/Form6251';  // TODO - revise for 2020, ripple f1040
+export { default as Form8606 } from '../fed2019/Form8606';
+export { default as Form8949 } from '../fed2019/Form8949';
+export { default as Form8959 } from '../fed2019/Form8959';
+export { default as Form8960 } from '../fed2019/Form8960';
+export { default as Form8995REIT } from '../fed2019/Form8995';  // TODO - check for 2020
+export { default as Schedule1 } from '../fed2019/Schedule1';
+export { default as Schedule2 } from '../fed2019/Schedule2';
+export { default as Schedule3 } from '../fed2019/Schedule3';  // TODO - revise for 2020
+export { default as ScheduleA } from '../fed2019/ScheduleA';  // TODO - revise for 2020, ripple f1040
+export { default as ScheduleD } from '../fed2019/ScheduleD';  // TODO - revise for 2020, ripple f1040
+export { default as W2 } from '../fed2019/W2';
+
+export * from '../fed2019/Form1040';  // TODO - revise for 2020
+export * from '../fed2019/Form1099B';
+export * from '../fed2019/Form1099R';
+export * from '../fed2019/Form1116';
+export * from '../fed2019/Form8949';
+export * from '../fed2019/Schedule1';
+export * from '../fed2019/ScheduleD';  // TODO - revise for 2020, ripple f1040
+export * from '../fed2019/W2';
 
 export { default as TaxReturn } from './TaxReturn';
